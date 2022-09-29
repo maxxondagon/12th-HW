7baf5b25b5a5166eb744c45c10ec03f634c9459f
Maxim Soloboev <fateless@vk.com>
2022-09-29 15:58:40 +0500

    Make simple button and label.

--- 12th-HW/Sources/App/ViewController.swift
+++ 12th-HW/Sources/App/ViewController.swift
@@ -12,10 +12,26 @@ class ViewController: UIViewController {
 
     // MARK: - Outlets
     
+    private lazy var timerlabel: UILabel = {
+        let timerLabel = UILabel()
+        timerLabel.text = "0)))"
+        timerLabel.translatesAutoresizingMaskIntoConstraints = false
+        return timerLabel
+    }()
+    
+    private lazy var startButton: UIButton = {
+        let timerButton = UIButton(type: .system)
+        timerButton.setTitle("Start", for: .normal)
+        timerButton.backgroundColor = .systemMint
+        timerButton.translatesAutoresizingMaskIntoConstraints = false
+        return timerButton
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         setupHierarchy()
         setupLayout()
     }
@@ -23,11 +39,20 @@ class ViewController: UIViewController {
     // MARK: - Setup
     
     private func setupHierarchy() {
-        
+        view.addSubview(timerlabel)
+        view.addSubview(startButton)
     }
     
     private func setupLayout() {
+        timerlabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
         
+        startButton.snp.makeConstraints { make in
+            make.top.equalTo(timerlabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
     }
     
     // MARK: - Actions
